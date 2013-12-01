04477f9c631711efa840177e8c1ad9fb9dbc38bd
Steve Persch <persch@palantir.net>
2013-11-30 18:01:43 -0600

    Adding a function for parsing single files

--- index.php
+++ index.php
@@ -14,8 +14,22 @@ function singleMessageParser($message) {
 
   $return['time'] = $messageCrawler->filter('div abbr')->attr('title');
   $return['sender_number'] = $messageCrawler->filter(' div cite a')->attr('href');
-  $return['sender_name'] = $messageCrawler->filter('div cite span.fn')->text();
+  $return['sender_name'] = $messageCrawler->filter('div cite .fn')->text();
   $return['message'] = $messageCrawler->filter('div q')->text();
 
   return $return;
+}
+
+
+function singleFileParser($uri) {
+  $return = array();
+
+  $contents = file_get_contents($uri);
+  $crawler = new Crawler($contents);
+
+  $messages = $crawler->filter('div.message')->each(function ($node, $i) {
+     return singleMessageParser($node);
+  });
+
+  return $messages;
 }
