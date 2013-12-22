9bc61c29c1346131f4c5dcb09f298fa8a4843415
Steve Persch <persch@palantir.net>
2013-12-22 12:43:19 -0600

    edits from a few weeks ago

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
-
+require 'vendor/autoload.php';
 use Symfony\Component\DomCrawler\Crawler;
 
+
+$test_dir = 'data_source';
 
 
 class singleMessageParser {
@@ -68,3 +70,75 @@ class singleFileParser {
   }
 }
 
+class directoryParser {
+
+  protected function __construct($directory_path) {
+
+
+  }
+
+
+}
+
+
+
+$scanned = scandir($test_dir);
+$all_messages = array();
+
+
+foreach ($scanned as $file_name) {
+
+  if (strpos($file_name, '.html') === (strlen($file_name)-5)) {
+
+    // @todo only caring about texts for now.
+
+
+    if (strpos($file_name, ' - Text - ')) {
+      $single_file_parser = new singleFileParser($test_dir . '/' . $file_name);
+      $derived_array = $single_file_parser->getOutputArray();
+      $all_messages = array_merge($all_messages, $derived_array);
+    }
+  }
+
+
+}
+
+
+$texts_from_me = array();
+foreach ($all_messages as $message) {
+  if ($message['sender_name'] === 'Me') {
+    $texts_from_me[] = $message;
+  }
+}
+
+
+$results = array();
+
+foreach ($texts_from_me as $message) {
+  $month = substr($message['time'], 0, 7);
+
+  if (!isset($results[$month])) {
+    $results[$month] = array(
+      'total_texts' => 0,
+      'texts_with_exclamation' => 0,
+    );
+  }
+
+  $results[$month]['total_texts']++;
+  if (strpos($message['message'], '!') !== FALSE) {
+    $results[$month]['texts_with_exclamation']++;
+  }
+}
+
+ksort($results);
+
+foreach($results as $month => $numbers) {
+  $ratio = $numbers['texts_with_exclamation']/$numbers['total_texts'];
+
+  $results[$month]['percentage'] = round($ratio, 2) * 100;
+
+  print_r($month);
+  print_r(',');
+  print_r(implode(',', $results[$month]));
+  print_r("\n");
+}
