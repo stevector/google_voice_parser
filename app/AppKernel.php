b7e4df114745894a1243af084ae937acc272c73f
Steve Persch <steve@stevepersch.com>
2013-12-27 23:04:39 -0800

    Adding altamirabundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new GoogleVoiceParser\FirstBundle\GoogleVoiceParserFirstBundle(),
             new APY\DataGridBundle\APYDataGridBundle(),
+            new Malwarebytes\AltamiraBundle\MalwarebytesAltamiraBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
